4afb441f90e214c04ef41ddb84a9f4d85a56957f
kholland4 <user@swift3.hollandnet.local>
2019-10-27 16:35:25 -0600

    placeholder conveyor textures

--- pipe_def.js
+++ pipe_def.js
@@ -6,14 +6,14 @@ definePipe(new PipeType(
 ));
 
 definePipe(new PipeType(
-  "pipe:conveyor", "Conveyor",
-  "tiles/default_wood_fence_", {build_cost: {wood: -0.5, stone: -0.5}, build_speed: 3},
+  "pipe:wood_conveyor", "Conveyor",
+  "tiles/default_pipe_wood_conveyor_", {build_cost: {wood: -0.5, stone: -0.5}, build_speed: 3},
   {density: {max: 9, gain: 0.06}, corrosion: {max: 0.5, gain: 0.1}},
   ["solid", "dust"]
 ));
 
 definePipe(new PipeType(
-  "pipe:axle", "Axle",
+  "pipe:wood_axle", "Axle",
   "tiles/default_wood_fence_", {build_cost: {wood: -0.75}, build_speed: 3},
   {energy: {max: 3, gain: 0.1}},
   ["mech_energy"]
